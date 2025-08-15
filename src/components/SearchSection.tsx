2d2d2662fa81f57ebda7938b51d0bf2300ce84b8
choihooo <hochoi8621@naver.com>
2025-08-15 18:27:12 +0900

    feat: 모바일 헤더 사이드바 구현

--- src/components/SearchSection.tsx
+++ src/components/SearchSection.tsx
@@ -6,8 +6,10 @@ export default function SearchSection() {
       <div className="flex w-full flex-col items-center gap-5 pt-5 md:pt-0">
         <div className="flex flex-col items-center gap-1 text-[18px] font-bold md:flex-row md:text-[22px]">
           <span>대기업 및 유니콘 채용 공고를</span>
-          <span className="text-[#6F00B6]">빠짐없이 모두</span>
-          <span> 모았어요.</span>
+          <div className="flex items-center">
+            <span className="text-[#6F00B6]">빠짐없이 모두</span>
+            <span>&nbsp;모았어요.</span>
+          </div>
         </div>
         <section className="flex w-full flex-1 items-center justify-center px-4 md:px-0">
           <div className="relative flex items-center justify-between h-14 rounded-2xl border-2 border-[#EDEDED] bg-[#F7F7F7] p-5 w-full text-[#999999] md:max-w-[640px]">
