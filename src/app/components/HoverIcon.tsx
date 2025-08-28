6d399235f2d0f607cae929d53ddfd6e3d5b9e53b
betteree <kkne1017@gmail.com>
2025-08-28 22:21:41 +0900

    feat: 메모장 아이콘

--- src/app/components/HoverIcon.tsx
+++ src/app/components/HoverIcon.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 type IconProps = React.SVGProps<SVGSVGElement> & {
-  variant: "trash" | "plus" | "edit" | "right";
+  variant:
+    | "trash"
+    | "plus"
+    | "edit"
+    | "right"
+    | "maximize"
+    | "more"
+    | "minimize";
 };
 
 export default function HoverIcon({ variant, ...props }: IconProps) {
@@ -9,24 +16,42 @@ export default function HoverIcon({ variant, ...props }: IconProps) {
     case "trash":
       return (
         <svg
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+          focusable="false"
           {...props}
         >
-          <rect
-            x="8"
-            y="3"
-            width="8"
-            height="3"
-            rx="0.666"
-            fill="currentColor"
+          <path
+            d="M3.375 5.625H4.625H14.625"
+            stroke="currentColor"
+            strokeWidth={1.25}
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
           <path
-            d="M19.333 6.60059C19.7011 6.60058 19.9999 6.89849 20 7.2666V8.55176C19.9999 8.91985 19.7011 9.21777 19.333 9.21777H19.1279C18.7599 9.21796 18.462 9.51674 18.4619 9.88477V20.333C18.4619 20.7012 18.1631 21 17.7949 21H6.5127C6.14452 21 5.8457 20.7012 5.8457 20.333V9.88574C5.8457 9.51765 5.54775 9.2189 5.17969 9.21875H4.66699C4.2988 9.21875 4 8.91995 4 8.55176V7.26758C4 6.8994 4.29881 6.60059 4.66699 6.60059H19.333ZM10.1523 17.1885H11.0752V10.4121H10.1523V17.1885ZM12.9219 10.4121V17.1885H13.8447V10.4121H12.9219Z"
-            fill="currentColor"
+            d="M13.5 5.625V14.5125C13.5 14.8407 13.3645 15.1555 13.1234 15.3875C12.8823 15.6196 12.5553 15.75 12.2143 15.75H5.78571C5.44472 15.75 5.1177 15.6196 4.87658 15.3875C4.63546 15.1555 4.5 14.8407 4.5 14.5125V5.625M6.42857 5.625V3.4875C6.42857 3.15929 6.56403 2.84453 6.80515 2.61246C7.04627 2.38038 7.37329 2.25 7.71429 2.25H10.2857C10.6267 2.25 10.9537 2.38038 11.1949 2.61246C11.436 2.84453 11.5714 3.15929 11.5714 3.4875V5.625"
+            stroke="currentColor"
+            strokeWidth={1.25}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M7.875 9V12.375"
+            stroke="currentColor"
+            strokeWidth={1.25}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M10.125 9V12.375"
+            stroke="currentColor"
+            strokeWidth={1.25}
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
         </svg>
       );
@@ -88,6 +113,121 @@ export default function HoverIcon({ variant, ...props }: IconProps) {
           <path
             d="M8.99953 8.12441L12.8795 12.0044L8.99953 15.8844C8.60953 16.2744 8.60953 16.9044 8.99953 17.2944C9.38953 17.6844 10.0195 17.6844 10.4095 17.2944L14.9995 12.7044C15.3895 12.3144 15.3895 11.6844 14.9995 11.2944L10.4095 6.70442C10.0195 6.31441 9.38953 6.31441 8.99953 6.70442C8.61953 7.09442 8.60953 7.73441 8.99953 8.12441Z"
             fill="currentColor"
+          />
+        </svg>
+      );
+
+    case "maximize":
+      return (
+        <svg
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M10 2H14V6"
+            stroke="#303030"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M6 14H2V10"
+            stroke="#303030"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M14 2L9 7"
+            stroke="#303030"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M2 14L7 9"
+            stroke="#303030"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      );
+
+    case "more":
+      return (
+        <svg
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M9.9974 10.8327C10.4576 10.8327 10.8307 10.4596 10.8307 9.99935C10.8307 9.53911 10.4576 9.16602 9.9974 9.16602C9.53716 9.16602 9.16406 9.53911 9.16406 9.99935C9.16406 10.4596 9.53716 10.8327 9.9974 10.8327Z"
+            stroke="#303030"
+            strokeWidth="1.66667"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M9.9974 5.00065C10.4576 5.00065 10.8307 4.62756 10.8307 4.16732C10.8307 3.70708 10.4576 3.33398 9.9974 3.33398C9.53716 3.33398 9.16406 3.70708 9.16406 4.16732C9.16406 4.62756 9.53716 5.00065 9.9974 5.00065Z"
+            stroke="#303030"
+            strokeWidth="1.66667"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M9.9974 16.6667C10.4576 16.6667 10.8307 16.2936 10.8307 15.8333C10.8307 15.3731 10.4576 15 9.9974 15C9.53716 15 9.16406 15.3731 9.16406 15.8333C9.16406 16.2936 9.53716 16.6667 9.9974 16.6667Z"
+            stroke="#303030"
+            strokeWidth="1.66667"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      );
+    case "minimize":
+      return (
+        <svg
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+          focusable="false"
+          {...props}
+        >
+          <path
+            d="M13 7H9V3"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M3 9H7V13"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M9 7L14 2"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M7 9L2 14"
+            stroke="currentColor"
+            strokeWidth="1.5"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
         </svg>
       );
