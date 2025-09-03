9e0a9121ca62bf99cbc13f98bdc459e6c8ef1f3d
betteree <kkne1017@gmail.com>
2025-09-03 14:29:24 +0900

    chore: 경로에서 api 제외

--- next.config.ts
+++ next.config.ts
@@ -21,7 +21,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/:path*`,
       },
     ];
   },
