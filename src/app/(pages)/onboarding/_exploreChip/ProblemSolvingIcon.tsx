3e8c6ccc9a2c49d5efa1c02f1bf49ea723a08b92
betteree <kkne1017@gmail.com>
2025-09-07 11:53:16 +0900

    feat: 아이콘 컴포넌트 추가

---
+++ src/app/(pages)/onboarding/_exploreChip/ProblemSolvingIcon.tsx
@@ -0,0 +1,69 @@
+interface ProblemSolvingIconProps {
+  isSelected?: boolean;
+}
+
+export function ProblemSolvingIcon({
+  isSelected = false,
+}: ProblemSolvingIconProps) {
+  return (
+    <svg
+      width={52}
+      height={52}
+      viewBox="0 0 50 50"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      overflow="visible" // 🔸 빛 번짐이 잘리지 않도록
+      aria-hidden="true"
+      focusable="false"
+    >
+      {/* 바깥 글로우: 화이트 하이라이트 + 에메랄드 글로우 2겹 */}
+      <g
+        style={
+          isSelected
+            ? {
+                filter:
+                  "drop-shadow(0 0 6px rgba(255,255,255,0.9)) " +
+                  "drop-shadow(0 0 18px rgba(28,198,180,0.55)) " +
+                  "drop-shadow(0 0 28px rgba(28,198,180,0.28))",
+              }
+            : undefined
+        }
+      >
+        {/* 안쪽 자체 밝기 업: 색을 더 환하게/선명하게 */}
+        <g
+          style={
+            isSelected
+              ? {
+                  filter: "brightness(1.12) saturate(1.18) contrast(1.05)",
+                }
+              : undefined
+          }
+        >
+          <rect
+            x="-0.00366211"
+            width="50"
+            height="50"
+            rx="8.33333"
+            fill="#B5EEE8"
+          />
+          <path
+            d="M31.5946 10.4142C32.3756 9.63317 33.642 9.63317 34.423 10.4142L40.0799 16.0711C40.8609 16.8521 40.8609 18.1184 40.0799 18.8995L21.6951 37.2843L13.2098 28.799L31.5946 10.4142Z"
+            fill="#009F8E"
+          />
+          <path
+            d="M13.208 28.7988L21.6933 37.2841L10.7331 39.0519L13.208 28.7988Z"
+            fill="white"
+          />
+          <path
+            d="M12.2354 32.7739L17.3978 37.9364L10.7355 39.0523L12.2354 32.7739Z"
+            fill="#074F47"
+          />
+          <path
+            d="M31.5946 10.4142C32.3756 9.63316 33.642 9.63317 34.423 10.4142L40.0799 16.0711C40.8609 16.8521 40.8609 18.1184 40.0799 18.8995L35.8372 23.1421L27.3519 14.6569L31.5946 10.4142Z"
+            fill="#1CC6B4"
+          />
+        </g>
+      </g>
+    </svg>
+  );
+}
