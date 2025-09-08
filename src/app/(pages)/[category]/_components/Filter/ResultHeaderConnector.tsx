2733fb5a487a4d5676e0b675ef74bf64df8245c5
betteree <kkne1017@gmail.com>
2025-09-08 12:10:42 +0900

    feat: 필터링 기업 갯수

--- src/app/(pages)/[category]/_components/Filter/ResultHeaderConnector.tsx
+++ src/app/(pages)/[category]/_components/Filter/ResultHeaderConnector.tsx
@@ -2,6 +2,10 @@
 
 import * as React from "react";
 import ResultHeader from "./ResultHeader";
+import { useFilterDialog } from "@/app/(pages)/[category]/_hooks/useFilterDialog";
+import { useMemo } from "react";
+import { mapFiltersToParams } from "@/app/_utils/mapFiltersToParams";
+import { useRecruitments } from "@/app/_api/recruitment/useRecruitments";
 
 type ResultHeaderConnectorProps = {
   total?: number;
@@ -10,10 +14,15 @@ type ResultHeaderConnectorProps = {
 };
 
 export default function ResultHeaderConnector({
-  total,
   showTodayBadge = true,
   className = "",
 }: ResultHeaderConnectorProps) {
+  const { filters } = useFilterDialog();
+  const params = useMemo(() => mapFiltersToParams(filters), [filters]);
+  const { data } = useRecruitments({ ...params, page: 0, size: 1 });
+
+  const total = data?.data.page.totalElements ?? 0;
+
   return (
     <ResultHeader
       total={total}
