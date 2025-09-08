80287fefc1e873eee5b9e864c89e6697a061a78a
choihooo <hochoi8621@naver.com>
2025-09-08 11:08:44 +0900

    fix: svg 코드 수정

--- src/components/Icons/Profile.tsx
+++ src/components/Icons/Profile.tsx
@@ -8,11 +8,11 @@ export function Profile() {
       xmlns="http://www.w3.org/2000/svg"
     >
       <circle cx="16" cy="16" r="16" fill="#EBDCF5" />
-      <circle cx="16" cy="11" r="3" stroke="#6F00B6" stroke-width="2" />
+      <circle cx="16" cy="11" r="3" stroke="#6F00B6" strokeWidth="2" />
       <path
         d="M9 24V22C9 19.7909 10.7909 18 13 18H19C21.2091 18 23 19.7909 23 22V24"
         stroke="#6F00B6"
-        stroke-width="2"
+        strokeWidth="2"
       />
     </svg>
   );
