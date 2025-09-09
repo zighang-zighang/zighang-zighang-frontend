d38439c65073d9a729c3a57c07d835ac790d238a
choihooo <hochoi8621@naver.com>
2025-09-10 03:03:29 +0900

    feat: 온보딩 퍼널에 intro 추가 및 최근 필터 적용 컨텍스트 타입 업데이트

---
+++ src/app/(pages)/onboarding/_funnel/steps/IntroStep.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import { StepContainer } from "../../_components/Container/StepContainer";
+import { ActionButton } from "../../_components/Button/ActionButton";
+
+export function IntroStep({
+  onNext,
+}: {
+  onNext: (applyRecentFilter: boolean) => void;
+}) {
+  const [applyRecent, setApplyRecent] = React.useState(false);
+  return (
+    <StepContainer>
+      <div className="relative w-full h-full rounded-[16px]">
+        <Image
+          src="/Onboardingbackground.png"
+          alt="background"
+          fill
+          className="object-cover"
+          priority
+        />
+
+        <div className="w-full h-full absolute inset-0 flex flex-col items-center pt-[62px] pb-[30px]">
+          <p className="text-Heading3-18sb mb-1 text-[#5E5E5F]">
+            직행, 뻔한 채용공고 사이트 아니에요.
+          </p>
+          <h2 className="text-Heading1-24sb text-black mb-[255px]">
+            오직 당신 ‘키워드’의 맞춤형 공고를 빠르게 찾아드려요!
+          </h2>
+
+          <label className="flex items-center gap-2 text-[#363636] text-[16px] mb-6">
+            <input
+              type="checkbox"
+              className="w-5 h-5"
+              checked={applyRecent}
+              onChange={(e) => setApplyRecent(e.target.checked)}
+            />
+            최근 설정한 필터 적용
+          </label>
+          <ActionButton onClick={() => onNext(applyRecent)} state="abled">
+            시작하기
+          </ActionButton>
+        </div>
+      </div>
+    </StepContainer>
+  );
+}
