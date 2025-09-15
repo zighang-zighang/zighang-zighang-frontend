66ba58962e39de225718ebd6d1da59d0c96d2b25
betteree <kkne1017@gmail.com>
2025-09-15 21:08:12 +0900

    style: 모바일 하단 버튼 그림자 추가

--- src/app/(pages)/onboarding/_components/Button/MobileBottomActions.tsx
+++ src/app/(pages)/onboarding/_components/Button/MobileBottomActions.tsx
@@ -17,8 +17,10 @@ export function ResponsiveStepActions({
     <>
       {/* 모바일: 하단 고정 */}
       <div
-        className={`fixed bottom-0 left-0 right-0 z-50 bg-white p-3 md:hidden ${
-          showBorder ? "border-t border-gray-200" : ""
+        className={`fixed bottom-0 left-0 right-0 z-50 bg-white p-3 md:hidden  ${
+          showBorder
+            ? "border-t border-gray-200 shadow-[0_-6px_6px_-1px_rgba(0,0,0,0.04)]"
+            : ""
         } ${className}`}
       >
         <div className="flex  items-center justify-center gap-2 ">
